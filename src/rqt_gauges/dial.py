7133b0f295db8d6ef79d99ab87173c33b1cf1844
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-02-20 10:19:37 +0100

    Rebase from 'release/humble/rqt_gauges'

--- src/rqt_gauges/dial.py
+++ src/rqt_gauges/dial.py
@@ -1,16 +1,16 @@
 from qt_gui.plugin import Plugin
 
-from .steering_wheel_widget import SteeringWheelWidget
+from .dial_widget import DialWidget
 
 
-class SteeringWheel(Plugin):
+class Dial(Plugin):
 
     def __init__(self, context):
         super().__init__(context)
-        self.setObjectName('Speedometer')
+        self.setObjectName('Dial')
 
         self._context = context
         self._node = context.node
 
-        self._widget = SteeringWheelWidget(self._node)
+        self._widget = DialWidget(self._node)
         context.add_widget(self._widget)
